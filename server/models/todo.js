efd9029358825a29a35718228351296cabaf4749
Ozgun <ozgun.abanoz@gmail.com>
2019-01-22 13:05:00 +0300

    Add post todos and refactor mongoose

---
+++ server/models/todo.js
@@ -0,0 +1,34 @@
+var mongoose = require('mongoose');
+
+var Todo = mongoose.model('Todo', {
+
+    text: {
+
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true // clears white spaces
+
+    },
+
+    completed: {
+
+        type: Boolean,
+        default: false
+
+    },
+
+    completedAt: {
+
+        type: Number,
+        default: null
+
+    }
+
+});
+
+module.exports = {
+
+    Todo
+
+};
---
+++ server/models/user.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+
+var Users = mongoose.model('Users', {
+
+    email: {
+
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 1
+
+    }
+
+});
+
+module.exports = {
+
+    Users
+
+};
