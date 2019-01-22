094502841c676bd2252f73671d263db9e09ea5ac
Ozgun <ozgun.abanoz@gmail.com>
2019-01-22 15:09:24 +0300

    Added get

--- server/server.js
+++ server/server.js
@@ -29,6 +29,20 @@ app.post('/todos', (req, res) => {
 
 });
 
+app.get('/todos', (req, res) => {
+
+    Todo.find().then((todos) => {
+
+        res.send({todos});
+
+    }, (err) => {
+
+        res.status(400).send(err);
+
+    });
+    
+});
+
 app.listen(3000, () => {
 
     console.log('Starting to listen');
