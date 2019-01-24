129de3a75c5c99192a8f82996c9db3673ce9dfc7
Ozgun <ozgun.abanoz@gmail.com>
2019-01-24 08:24:15 +0300

    added test for get/todos/id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,15 +1,18 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo.js');
 
 const todos = [{
 
+    _id: new ObjectID(),
     text: 'First test to do'
 
 }, {
 
+    _id: new ObjectID(),
     text: 'Second test to do'
 
 }];
@@ -102,6 +105,46 @@ describe('GET /todos', () => {
             })
             .end(done);
 
+    });
+
+});
+
+describe('GET /todos/:id', () => {
+
+    it('should return todo doc', (done) => {
+
+        request(app)
+            .get(`/todos/${todos[0]._id.toHexString()}`)
+            .expect(200)
+            .expect((res) => {
+
+                // console.log(res.body.todo.text);
+                expect(res.body.todo.text).toBe(todos[0].text);
+
+            })
+            .end(done);
+
+    });
+
+    it('should return a 404 if todo not found', (done) => {
+
+        var tempId = new ObjectID();
+
+        request(app)
+            .get(`/todos/${tempId.toHexString()}`)
+            .expect(404)
+            .end(done);
+
+    });
+
+    it('should return 404 when non object ids', (done) => {
+
+        var tempId = '456';
+
+        request(app)
+            .get('/todos/'+tempId)
+            .expect(404)
+            .end(done);
     });
 
 });
