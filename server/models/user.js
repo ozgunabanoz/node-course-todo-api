a8280d3e191b38a77d74ae1f2c7e11a9ea1437cb
Ozgun <ozgun.abanoz@gmail.com>
2019-01-25 09:37:42 +0300

    add post/users and token method

--- server/models/user.js
+++ server/models/user.js
@@ -1,17 +1,81 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-var Users = mongoose.model('Users', {
+var UserSchema = new mongoose.Schema({
 
     email: {
 
         type: String,
         required: true,
         trim: true,
-        minlength: 1
+        minlength: 1,
+        unique: true,
+        validate: {
 
-    }
+            validator: validator.isEmail,
+
+            message: '{VALUE} is not a valid email'
+
+        }
+
+    },
+
+    password: {
+
+        type: String,
+        required: true,
+        minlength: 6
+
+    },
+
+    tokens: [{
+
+        access: {
+
+            type: String,
+            require: true
+
+        },
+
+        token: {
+
+            type: String,
+            require: true
+
+        }
+
+    }]
 
 });
+
+UserSchema.methods.toJSON = function () {
+
+    var user = this;
+    var userObject = user.toObject();
+
+    return _.pick(userObject, ['_id', 'email']);
+
+};
+
+UserSchema.methods.generateAuthToken = function () {
+
+    var user = this;
+    var access = 'auth';
+    var token = jwt.sign({_id: user._id.toString(), access}, 'abc123').toString();
+
+    user.tokens = user.tokens.concat([{access, token}]);
+
+    return user.save().then(() => {
+
+        return token;
+
+    });
+
+};
+
+var Users = mongoose.model('Users', UserSchema);
 
 module.exports = {
 
--- server/server.js
+++ server/server.js
@@ -34,6 +34,30 @@ app.post('/todos', (req, res) => {
 
 });
 
+// POST users
+
+app.post('/users', (req, res) => {
+
+    var body = _.pick(req.body, ['email', 'password']);
+
+    var user = new Users(body);
+
+    user.save().then((user) => {
+
+        return user.generateAuthToken();
+
+    }).then((token) => {
+
+        res.header('x-auth', token).send(user);
+
+    }).catch((e) => {
+
+        res.status(400).send(e);
+
+    });
+
+});
+
 app.get('/todos', (req, res) => {
 
     Todo.find().then((todos) => {
